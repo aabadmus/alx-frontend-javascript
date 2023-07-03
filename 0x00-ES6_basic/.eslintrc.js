26c72bb2a0277cc23b573cb1fc34f18386f7af3c
Ahmed Adekunle Badmus <aabadmus@yahoo.com>
2023-07-03 03:46:50 +0100

    promise

---
+++ 0x00-ES6_basic/.eslintrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true,
+    jest: true
+  },
+  extends: ["eslint:recommended"],
+  parserOptions: {
+    ecmaVersion: 2020
+  },
+  rules: {
+    // Add your ESLint rules here
+  }
+};
+
